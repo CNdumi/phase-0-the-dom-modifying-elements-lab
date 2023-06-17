4bc50a0a82496e4925b8308e0df20c13e333265b
CNdumi <carolinendumi12@gmail.com>
2023-06-17 23:37:54 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+document.getElementById('main').remove()
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = 'Carole is the champion';
+newHeader.body.append(newHeader);
+
+// var newElement = document.createElement('div');
+
+// // Assign an ID to the element
+// newElement.id = 'myElement';
+
+// // Append the element to the body of the document
+// document.body.appendChild(newElement);
